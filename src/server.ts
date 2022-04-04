67b6e3378ac1bbaa5b7b7f405c176b36681b125c
Victor Hugo <valvesperricci@yahoo.com.br>
2022-04-04 09:47:48 -0300

    adicionando swagger para documentar a api

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from './swagger.json';
 
 // routes
 import { routes } from "./routes/index.routes";
 
 const app = express();
-
 app.use(express.json());
-app.use(routes);
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(routes);
 app.listen(3333);
